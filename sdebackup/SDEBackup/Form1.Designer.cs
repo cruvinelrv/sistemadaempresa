5003c446b2971c81482b3571cdda642ffb7b0ba3
cruvinelrv <vinicius@inatos.com.br>
2022-01-03 10:52:13 -0300

    sde backup

---
+++ sdebackup/SDEBackup/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace SDEBackup
+{
+    partial class frmSDEBackup
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmSDEBackup));
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnBackupLocal = new System.Windows.Forms.Button();
+            this.btnFazerBackup = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtBackup = new System.Windows.Forms.TextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.progressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.status = new System.Windows.Forms.ToolStripStatusLabel();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolStripSplitButton1 = new System.Windows.Forms.ToolStripSplitButton();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(18, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 16);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Salvar backup em:";
+            // 
+            // btnBackupLocal
+            // 
+            this.btnBackupLocal.Image = ((System.Drawing.Image)(resources.GetObject("btnBackupLocal.Image")));
+            this.btnBackupLocal.Location = new System.Drawing.Point(444, 14);
+            this.btnBackupLocal.Name = "btnBackupLocal";
+            this.btnBackupLocal.Size = new System.Drawing.Size(36, 30);
+            this.btnBackupLocal.TabIndex = 9;
+            this.btnBackupLocal.UseVisualStyleBackColor = true;
+            this.btnBackupLocal.Click += new System.EventHandler(this.btnBackupLocal_Click);
+            // 
+            // btnFazerBackup
+            // 
+            this.btnFazerBackup.Image = ((System.Drawing.Image)(resources.GetObject("btnFazerBackup.Image")));
+            this.btnFazerBackup.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnFazerBackup.Location = new System.Drawing.Point(347, 323);
+            this.btnFazerBackup.Name = "btnFazerBackup";
+            this.btnFazerBackup.Size = new System.Drawing.Size(135, 43);
+            this.btnFazerBackup.TabIndex = 8;
+            this.btnFazerBackup.Text = "Fazer &Backup";
+            this.btnFazerBackup.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnFazerBackup.UseVisualStyleBackColor = true;
+            this.btnFazerBackup.Click += new System.EventHandler(this.btnFazerBackup_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle5.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle5.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle5;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dataGridView1.Location = new System.Drawing.Point(18, 64);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(460, 244);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // Column1
+            // 
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Column1.DefaultCellStyle = dataGridViewCellStyle6;
+            this.Column1.HeaderText = "Pastas a serem copiadas";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 380;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Copia Servidor";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // txtBackup
+            // 
+            this.txtBackup.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.txtBackup.Location = new System.Drawing.Point(18, 19);
+            this.txtBackup.Name = "txtBackup";
+            this.txtBackup.ReadOnly = true;
+            this.txtBackup.Size = new System.Drawing.Size(405, 20);
+            this.txtBackup.TabIndex = 6;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripSplitButton1,
+            this.progressBar1,
+            this.status});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 373);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(497, 22);
+            this.statusStrip1.TabIndex = 11;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 16);
+            this.progressBar1.Visible = false;
+            // 
+            // status
+            // 
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(0, 17);
+            this.status.Visible = false;
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "ZIP|*.zip";
+            this.saveFileDialog1.Title = "Salvar Backup...";
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.IsBalloon = true;
+            this.toolTip1.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Warning;
+            this.toolTip1.ToolTipTitle = "Atenção";
+            // 
+            // toolStripSplitButton1
+            // 
+            this.toolStripSplitButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripSplitButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripSplitButton1.Image")));
+            this.toolStripSplitButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripSplitButton1.Name = "toolStripSplitButton1";
+            this.toolStripSplitButton1.Size = new System.Drawing.Size(32, 20);
+            this.toolStripSplitButton1.Text = "toolStripSplitButton1";
+            this.toolStripSplitButton1.Click += new System.EventHandler(this.toolStripSplitButton1_Click);
+            // 
+            // frmSDEBackup
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(497, 395);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnBackupLocal);
+            this.Controls.Add(this.btnFazerBackup);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtBackup);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmSDEBackup";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SDE Backup";
+            this.Load += new System.EventHandler(this.frm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnBackupLocal;
+        private System.Windows.Forms.Button btnFazerBackup;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.TextBox txtBackup;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripProgressBar progressBar1;
+        private System.Windows.Forms.ToolStripStatusLabel status;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.ToolStripSplitButton toolStripSplitButton1;
+    }
+}
+
---
+++ sdebackup/SDEBackup/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using FTP;
+
+
+namespace SDEBackup
+{
+    public partial class frmSDEBackup : Form
+    {
+        public funcoes func = new funcoes();
+        public ftp FTP = new ftp();
+        public string data, empresa;
+        public int nArqServidor;
+
+        public frmSDEBackup()
+        {
+            InitializeComponent();
+            func.downloadConfigBackup();
+        }
+        
+        private void frm_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Directory.Exists(@"C:\Documentos SDE\tmp\"))
+                {
+                    Directory.Delete(@"C:\Documentos SDE\tmp\", true);
+                }
+
+                func.lerConfigBackup(@"C:\Documentos SDE\configBackup.xml", dataGridView1);
+
+                empresa = func.empresa.ToString();
+            }
+            catch (Exception ex)
+            {
+                //
+            }
+        }
+
+        /// <summary>
+        /// Gera estrutura de pastas temporarias
+        /// </summary>
+        public void GeraTmp()
+        {
+            #region verifica se existem as pastas
+
+            if (!Directory.Exists(@"C:\Documentos SDE\tmp\SERVIDOR_ZIP\"))
+            {
+                Directory.CreateDirectory(@"C:\Documentos SDE\tmp\SERVIDOR_ZIP\");
+            }
+
+            if (!Directory.Exists(@"C:\Documentos SDE\tmp\LOCAL_ZIP\"))
+            {
+                Directory.CreateDirectory(@"C:\Documentos SDE\tmp\LOCAL_ZIP\");
+            }
+
+            #endregion
+
+            status.Text = "Gerando arquivos temporarios...";
+            Application.DoEvents();
+            //recebe a quantidade de linhas do datagridview
+            int cont = dataGridView1.Rows.Count;
+
+            progressBar1.Maximum = cont;
+
+            //laço de repetição do para gerar as copias da estrutura do datagridview
+            for (int i = 0; i < cont; i++)
+            {
+                string caminho = dataGridView1.Rows[i].Cells[0].Value.ToString();
+                bool copiaServidor = Convert.ToBoolean(dataGridView1.Rows[i].Cells[1].Value);
+
+                #region copia as pasta do backup no servidor
+
+                if (copiaServidor == true)
+                {
+                    try
+                    {
+                        DirectoryInfo pasta = new DirectoryInfo(caminho);
+                        string destino = @"C:\Documentos SDE\tmp\SERVIDOR_ZIP\" + pasta.Name.ToString();
+                        Directory.CreateDirectory(destino);
+                        func.DirectoryCopy(caminho, destino, true);
+                        nArqServidor += 1;
+                    }
+                    catch (Exception erro)
+                    {
+                        MessageBox.Show("ERRO:" + erro.Message);
+                    }
+                }
+
+                #endregion
+
+                #region copia as pasta do backup local
+
+                try
+                {
+                    DirectoryInfo pasta = new DirectoryInfo(caminho);
+                    string destino = @"C:\Documentos SDE\tmp\LOCAL_ZIP\" + pasta.Name.ToString();
+                    Directory.CreateDirectory(destino);
+                    func.DirectoryCopy(caminho, destino, true);
+                    progressBar1.Value = i;
+                }
+                catch (Exception erro)
+                {
+                    MessageBox.Show("ERRO:" + erro.Message);
+                }
+
+                #endregion
+
+                status.Text = "Arquivos temporarios gerados";
+                Application.DoEvents();
+            }
+        }
+
+        private void btnBackupLocal_Click(object sender, EventArgs e)
+        {
+            data = saveFileDialog1.FileName = DateTime.Now.ToString("dd_MM_yyyy HH_mm_ss");
+            saveFileDialog1.ShowDialog();
+
+            if (saveFileDialog1.FileName != data)
+            {
+                txtBackup.Text = saveFileDialog1.FileName;
+            }
+        }
+
+        private void btnFazerBackup_Click(object sender, EventArgs e)
+        {
+            string zipServidor;
+
+            if (txtBackup.Text != "")
+            {
+                status.Visible = true;
+                progressBar1.Visible = true;
+   
+                GeraTmp();
+
+                status.Text = "Compactando arquivo(s)...";
+                Application.DoEvents();
+
+                func.Compactar(@"C:\Documentos SDE\tmp\LOCAL_ZIP\", txtBackup.Text);
+
+                if (nArqServidor > 0)
+                {
+                    
+                    status.Text = "Compactando arquivo(s) para o servidor...";
+                    Application.DoEvents();
+
+                    func.Compactar(@"C:\Documentos SDE\tmp\SERVIDOR_ZIP\", zipServidor = @"C:\Documentos SDE\tmp\" + data + ".zip");
+
+                    status.Text = "Enviando arquivo(s) para o servidor...";
+                    Application.DoEvents();
+
+                    FTP.upload("Administrator", "paoc1710", zipServidor, "ftp://ftp.sistemadaempresa.com/backup/" + empresa + "/" + data + ".zip");
+                }
+
+                status.Text = "Backup Concluído!";
+                Application.DoEvents();
+
+                try
+                {   //apaga o diretorio temporario (tmp)
+                    Directory.Delete(@"C:\Documentos SDE\tmp\", true);
+                    progressBar1.Value += 1;
+                    MessageBox.Show("Backup efetuado com Sucesso!");
+                }
+                catch (Exception erro)
+                {
+                    MessageBox.Show("ERRO:" + erro.Message);
+                }
+
+                status.Visible = false;
+                progressBar1.Visible = false;
+            }
+            else
+            {
+                toolTip1.Show("Selecione a pasta para salvar o backup", btnBackupLocal, 2000);
+            }
+        }
+
+        private void toolStripSplitButton1_Click(object sender, EventArgs e)
+        {
+            AboutBox1 about = new AboutBox1();
+            about.ShowDialog();
+        }
+
+        
+    }
+}
